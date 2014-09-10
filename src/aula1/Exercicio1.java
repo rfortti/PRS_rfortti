0b0a8af033425ffcb0da7a70e157e58be7c291d5
Multas <Multas@192.168.1.32>
2014-09-10 15:49:26 -0300

    Merge origin/master
    Conflicts:
    	README.md

---
+++ src/aula1/Exercicio1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aula1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Aluno
+ */
+public class Exercicio1 
+{
+    /*Crie um algorítmo para ler dois números e retornar a soma desses números.
+    */
+ public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite o Primeiro Numero");
+        int num1 = scan.nextInt();
+        System.out.println("Digite o Segundo Numero");
+        int num2 = scan.nextInt();
+        
+        int soma = num1 + num2;
+        System.out.println("A soma do dois numeros eh = " + soma);
+    }
+}
